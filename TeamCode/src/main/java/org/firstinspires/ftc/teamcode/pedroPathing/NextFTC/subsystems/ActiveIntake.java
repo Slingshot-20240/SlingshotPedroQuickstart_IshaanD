faaa2c5bcc0012752ea89012ac485c4dad2b198c
ishaandesai <IshaanDesai21@gmail.com>
2025-08-02 22:35:25 -0600

    Removes PID Controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/NextFTC/subsystems/ActiveIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/NextFTC/subsystems/ActiveIntake.java
@@ -2,8 +2,6 @@ package org.firstinspires.ftc.teamcode.pedroPathing.NextFTC.subsystems;
 
 import com.rowanmcalpin.nextftc.core.Subsystem;
 import com.rowanmcalpin.nextftc.core.command.Command;
-import com.rowanmcalpin.nextftc.core.control.controllers.PIDFController;
-import com.rowanmcalpin.nextftc.core.control.controllers.feedforward.StaticFeedforward;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.SetPower;
 
@@ -12,7 +10,10 @@ public class ActiveIntake extends Subsystem {
     private ActiveIntake() { }
 
     public MotorEx activeIntake;
-    public PIDFController activeIntake_controller = new PIDFController(0.005, 0.0, 0.0, new StaticFeedforward(0.0));
+
+    //Remove intake PID Controller - Not necessary bc ur just setting power when creating Command Functions.
+    //You aren't using run to position
+    //public PIDFController activeIntake_controller = new PIDFController(0.005, 0.0, 0.0, new StaticFeedforward(0.0));
     private enum activeIntake_states {
         IN (0.7),
         IDLE (0),
