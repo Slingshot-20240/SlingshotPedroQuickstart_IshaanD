328ef902001ce8c5bb902375fd1bce09bd876ca9
ishaandesai <IshaanDesai21@gmail.com>
2025-08-29 22:52:15 -0500

    new WaitUntil(() -> !follower.isBusy())

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Pedro/constants/FConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Pedro/constants/FConstants.java
@@ -2,8 +2,7 @@ package org.firstinspires.ftc.teamcode.pedroPathing.Pedro.constants;
 
 import com.pedropathing.localization.Localizers;
 import com.pedropathing.follower.FollowerConstants;
-import com.pedropathing.util.CustomFilteredPIDFCoefficients;
-import com.pedropathing.util.CustomPIDFCoefficients;
+
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class FConstants {
